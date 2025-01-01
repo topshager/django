e228c43ca74eca3087ffabd9267b7157c7824be7
topshager <rem28777@gmail.com>
2025-01-01 21:41:18 +0200

    django view

---
+++ my_tennis_club/members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django .shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
