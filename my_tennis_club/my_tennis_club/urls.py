7d2e7286e3001eca466c36862f8e4ea277c8c6ef
topshager <rem28777@gmail.com>
2025-01-01 22:04:42 +0200

    migration ran

--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('members.urls')),
+    path('',include('tennis_app.urls')),
     path("admin/", admin.site.urls),
 ]
