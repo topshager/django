c1c00f2c5236f76269c6c2d1c984e833081d3b54
topshager <rem28777@gmail.com>
2025-01-03 17:59:26 +0200

    update admin.py

--- my_tennis_club/tennis_app/admin.py
+++ my_tennis_club/tennis_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+class MemeberAdmin(admin.ModelAdmin):
+    list_display = ("firstname","lastname","joined_date")
+admin.site.register(Member,MemeberAdmin)
