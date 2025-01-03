8be48577c92565065b180c62ef0321fd321cec3e
topshager <rem28777@gmail.com>
2025-01-03 18:06:34 +0200

    creating variable in template

--- my_tennis_club/tennis_app/views.py
+++ my_tennis_club/tennis_app/views.py
@@ -25,6 +25,6 @@ def main(request):
 def testing(request):
     template = loader.get_template('template.html')
     context = {
-        'fruits':['Apple','Banana','Cherry'],
+        'firstname': 'linus'
     }
     return HttpResponse(template.render(context, request))
