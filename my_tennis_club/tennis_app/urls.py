05c155481e5e27ff52d9eab522dff0c252d50a49
topshager <rem28777@gmail.com>
2025-01-03 17:37:04 +0200

    update urls.py

--- my_tennis_club/tennis_app/urls.py
+++ my_tennis_club/tennis_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.main, name='main'),
     path('tennis_app/', views.members, name='tennis_app'),
     path('tennis_app/details/<int:id>',views.details,name='details'),
+    path('testing/',views.testing, name='testing'),
 
 ]
