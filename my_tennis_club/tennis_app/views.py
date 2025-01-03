a400d3d1be5068d73cb7e76a2168d392280f79e4
topshager <rem28777@gmail.com>
2025-01-03 12:33:45 +0200

    updated view.py

--- my_tennis_club/tennis_app/views.py
+++ my_tennis_club/tennis_app/views.py
@@ -9,3 +9,12 @@ def members(request):
         'mymembers': mymembers,
     }
     return HttpResponse(template.render(context,request))
+
+def details(request,id):
+    mymember = Member.objects.all().values()
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context,request
+                                        ))
