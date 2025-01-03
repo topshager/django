5a6a8cc0da8bf1a611bd4afb371b14706ea84bff
topshager <rem28777@gmail.com>
2025-01-03 13:24:19 +0200

    added main to urls

--- my_tennis_club/tennis_app/urls.py
+++ my_tennis_club/tennis_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.main, name='main'),
     path('tennis_app/', views.members, name='tennis_app'),
     path('tennis_app/details/<int:id>',views.details,name='details'),
+
 ]
