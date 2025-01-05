3ce3792dac10437cb72d926919752c6183c3f0d3
topshager <rem28777@gmail.com>
2025-01-05 21:31:02 +0200

    update wsgi.py

--- my_tennis_club/my_tennis_club/wsgi.py
+++ my_tennis_club/my_tennis_club/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_tennis_club.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
