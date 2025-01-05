b8257f253514c3a80ffbfd6672550e74aead78f7
topshager <rem28777@gmail.com>
2025-01-05 20:38:26 +0200

    created virtual .env

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -123,6 +123,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_ROOT = BASE_DIR / 'productionfiles'
-
-STATIC_URL = 'static/'
